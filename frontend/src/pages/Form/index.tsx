3465724f4b86a8298c4dac0cebb05f06581eaf5e
lucasriber01 <lucasarago.df@gmail.com>
2022-03-10 01:23:46 -0300

    Routes

---
+++ frontend/src/pages/Form/index.tsx
@@ -0,0 +1,7 @@
+
+function Form() {
+    return (
+        <h1>Teste</h1>
+    );
+  }
+export default Form;
---
+++ frontend/src/pages/Listing/index.tsx
@@ -0,0 +1,7 @@
+
+function Listing() {
+    return (
+        <h1>Teste</h1>
+    );
+  }
+export default Listing;
